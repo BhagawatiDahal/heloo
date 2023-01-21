e2baf9b2796ef192d1e56630626e8897626ec6a5
BhagawatiDahal <85800287+BhagawatiDahal@users.noreply.github.com>
2023-01-20 23:14:37 -0600

    firstcommit

---
+++ src/MainClass.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MainClass {
+	/*
+	 * sjdskjhkhdsjk jdcnkzjnlkjls dnslkjdlklf
+	 */
+	public static void main(String[] args) {
+		System.out.println("ABC");
+		System.out.println("A");
+
+		String name = "Bhagawati";
+		int age = 36;
+		int id = 101;
+		System.out.println("name=" + name);
+		System.out.println("Age=" + age);
+
+		System.out.println("ID=" + id);
+
+		
+		Scanner sc= new Scanner(System.in );
+	}
+
+}
---
+++ src/Submain.java
@@ -0,0 +1,23 @@
+
+public class Submain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+float miles;
+float gallons;
+float mpg;
+miles= 300.04f;
+gallons=10f;
+
+
+
+mpg=miles/gallons;
+
+System.out.println("mpg=\t"+mpg +"\t");
+System.out.print("mpg=\t"+mpg);
+
+
+	}
+
+}
